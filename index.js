e3f75709b0ac07a0ab7e5e36ad3386c8f8d455d3
Harshit Singh Mahara <maharaharshit31@gmail.com>
2024-08-24 13:10:27 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const Blog = require("./models/blog");
 
 const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
 mongoose
   .connect("mongodb://localhost:27017/mindcanvas")
@@ -16,11 +26,15 @@ mongoose
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
 });
 
 app.use("/user", userRoute);
-
+app.use("/blog", blogRoute);
 
 app.listen(PORT, () => console.log(`Server Started at PORT ${PORT}...`));
